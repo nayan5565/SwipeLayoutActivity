b9f76146f78231c583e2ba12e3039b3d86d87fa3
nayan5565 <nayan5565@gmail.com>
2017-07-12 01:44:00 +0600

    ListView

--- app/src/main/java/com/example/nayan/swiperefresh/DataListActivity.java
+++ app/src/main/java/com/example/nayan/swiperefresh/DataListActivity.java
@@ -2,11 +2,15 @@ package com.example.nayan.swiperefresh;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 /**
  * Created by Nayan on 7/12/2017.
@@ -16,16 +20,25 @@ public class DataListActivity extends AppCompatActivity implements View.OnClickL
     TextView txt, txt_lst;
     Button btn_add, btn_update, btn_delete, btn_show;
     EditText edt_id, edt_name;
+    MList mList;
+    RecyclerView recyclerView;
+    ArrayList<MList> mLists;
+    AdapterList adapterList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.data_list);
         init();
+        prepareDisplay();
         getData();
     }
 
     private void init() {
+        adapterList = new AdapterList(this);
+        mLists = new ArrayList<>();
+        recyclerView = (RecyclerView) findViewById(R.id.rec);
+        mList = new MList();
         db = new DatabaseHelper(DataListActivity.this);
 
         txt = (TextView) findViewById(R.id.txt_op);
@@ -75,7 +88,10 @@ public class DataListActivity extends AppCompatActivity implements View.OnClickL
         int id = Integer.valueOf(value);
         String name = edt_name.getText().toString();
         result = db.addContuct(id, name, DatabaseHelper.TABLE_FRIENDS);
-
+        mList.setRank(id);
+        mList.setTitle(name);
+        mLists.add(mList);
+        adapterList.addAll(mLists);
 
         if (result < 0) {
             Toast.makeText(DataListActivity.this, "error " + result, Toast.LENGTH_SHORT).show();
@@ -119,5 +135,10 @@ public class DataListActivity extends AppCompatActivity implements View.OnClickL
     private void getData() {
         txt_lst.setText(db.getContuct());
 
+    }
+
+    private void prepareDisplay() {
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapterList);
     }
 }
